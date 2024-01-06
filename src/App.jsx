c637311bd7124a941a822f40b6f1fc4b0aab6c99
Devadharsan-k <kathirdharsan1@gmail.com>
2024-01-06 15:04:49 +0530

    chore: Initial commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Header from "./components/header/Header";
+import PageContent from "./components/page_content/PageContent";
+import Siderbar from "./components/sidebar/Siderbar";
+import "./App.css";
+
 function App() {
-  return <></>;
+  return (
+    <div className="page-layout">
+      <Siderbar />
+      <div className="main-content">
+        <Header />
+        <PageContent />
+      </div>
+    </div>
+  );
 }
 
 export default App;
